6ce55de88a94766e7d06e582ed515699928e2a9c
SayantanRC <sayantan.kgp@gmail.com>
2018-10-02 22:50:56 +0530

    tuning restoreScript to avoid errors

--- app/src/main/assets/restoreDataScript.sh
+++ app/src/main/assets/restoreDataScript.sh
@@ -1,13 +1,21 @@
 #!sbin/sh
 
-# parameters: TEMP_DIR_NAME tar.gz_file package_name app_uid
+# parameters: TEMP_DIR_NAME tar.gz_file package_name
+
 cp $1/$2 /data/data/
 cd /data/data/
-rm -rf $3
+
+dataDir=$(dumpsys package $3 | grep dataDir | head -n 1 | cut -d '=' -f2)
+rm -rf ${dataDir}
+
 $1/busybox tar -xzpf $2
 rm $2
-chmod 755 $3
+
 app_uid=$(dumpsys package $3 | grep userId= | cut -d '=' -f2)
-chown ${app_uid}:${app_uid} -Rf $3
-restorecon -RF $3
+chown ${app_uid}:${app_uid} -Rf ${dataDir}
+
+chmod 755 ${dataDir}
+
+restorecon -RF ${dataDir}
+
 rm -rf $1/$2
