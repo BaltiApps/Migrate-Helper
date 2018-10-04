7654753d83b99e6b77a21cc27f42c4f34d7340c0
SayantanRC <sayantan.kgp@gmail.com>
2018-10-04 23:47:44 +0530

    fixed errors in case of double appearance of userId.

--- app/src/main/assets/restoreDataScript.sh
+++ app/src/main/assets/restoreDataScript.sh
@@ -6,15 +6,18 @@ cp $1/$2 /data/data/
 cd /data/data/
 
 dataDir=$(dumpsys package $3 | grep dataDir | head -n 1 | cut -d '=' -f2)
+app_uid=$(dumpsys package $3 | grep userId= | head -n 1 | cut -d '=' -f2)
+
+# sometimes app_uid has two lines. example: Google maps (com.google.android.apps.maps)
+# so "head -n 1" takes only the first line.
+
 rm -rf ${dataDir}
 
 $1/busybox tar -xzpf $2
 rm $2
 
-app_uid=$(dumpsys package $3 | grep userId= | cut -d '=' -f2)
-chown ${app_uid}:${app_uid} -Rf ${dataDir}
-
 chmod 755 ${dataDir}
+chown ${app_uid}:${app_uid} -Rf ${dataDir}
 
 restorecon -RF ${dataDir}
 
