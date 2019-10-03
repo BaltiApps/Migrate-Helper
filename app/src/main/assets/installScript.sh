14feb9d394eb109216b0cc86a21148b42b7c2c67
SayantanRC <sayantan.kgp@gmail.com>
2019-10-03 23:54:29 +0530

    optimised scripts. install session for split apks.
    Signed-off-by: SayantanRC <sayantan.kgp@gmail.com>

--- app/src/main/assets/installScript.sh
+++ app/src/main/assets/installScript.sh
@@ -2,36 +2,64 @@
 
 # parameters:
 
-# TEMP_DIR_NAME_OLD
-# TEMP_DIR_NAME_NEW
-# apk_dir_name
-# app_name.apk
-# package_name
+TEMP_DIR_NAME=$1
+apk_dir_name=$2
+base_apk_name=$3
+package_name=$4
+installer_name=$5
 
+if [[ -e ${TEMP_DIR_NAME}/${apk_dir_name} ]]; then
 
-if [ -e $2/$3 ]; then
-
-    #split apk support from v2.0
-    full_apk_dir=$2/$3
+    full_apk_dir=${TEMP_DIR_NAME}/${apk_dir_name}
 
     chmod 777 "$full_apk_dir"
     cd ${full_apk_dir}
 
-    pm install -r -d -t ${4}
+    #install main app
+    if [[ -n ${installer_name} && ${installer_name} != "NULL" ]]; then
+        pm install -r -d -t -i ${installer_name} ${base_apk_name}
+    else
+        pm install -r -d -t ${base_apk_name}
+    fi
 
-    for filename in ${full_apk_dir}/split_*.apk; do
-        pm install -p $5 "$filename" 2>/dev/null && echo "Split: ${filename}"
-    done
+    split_count=$(ls -1 | grep "split" | grep ".apk" | wc -l)
+
+    if [[ ${split_count} -gt 0 && -n "$(pm list packages ${package_name})" ]]; then
+
+        ############## Made by Vijay ##############
+
+        # create install session
+        echo "Split: Creating Install Session"
+        if [[ -n ${installer_name} && ${installer_name} != "NULL" ]]; then
+            session=$(pm install-create -i ${installer_name} -p ${package_name} | cut -d'[' -f2 | cut -d']' -f1)
+        else
+            session=$(pm install-create -p ${package_name} | cut -d'[' -f2 | cut -d']' -f1)
+        fi
+
+        # add split apks
+        echo "Split: Adding Split Apks to Session"
+        for filename in ${full_apk_dir}/split_*.apk; do
+            pm install-write ${session} ${filename} ${full_apk_dir}/${filename} 2>/dev/null && echo "Split: Added ${filename}"
+        done
+
+        echo "Split: Installing Split Session (Be patient, it may take longer)"
+        pm install-commit ${session}
+        echo "Split: Done"
+
+        ############## Made by Vijay ##############
+
+    else
+        echo "No split apks. Split count: $split_count"
+    fi
+
+    sleep 0.5s
+
+    if [[ -z "$(pm list packages ${package_name})" ]]; then
+        echo "App not installed. Please install manually via Google Play or elsewhere"
+    fi
 
     rm -rf "$full_apk_dir"
 
-elif [ -e $1/$4 ]; then
-
-    # legacy support
-    cd ${1}
-    pm install -r -d -t ${4}
-    rm -r ${4}
-
 else
-    echo "Apk ${4} not found!"
+    echo "Apk(s) for ${package_name} not found!"
 fi
--- app/src/main/assets/restoreDataScript.sh
+++ app/src/main/assets/restoreDataScript.sh
@@ -2,49 +2,51 @@
 
 # parameters:
 
-# TEMP_DIR_NAME_OLD
-# TEMP_DIR_NAME_NEW
-# tar.gz_file
-# package_name
+TEMP_DIR_NAME=$1
+tar_gz_file=$2
+package_name=$3
 
-#   Starting from v1.2 tar.gz files are unpacked under /data/data
-
-#mv $1/$2 /data/data/
-# Changed from v1.2
-
-dataDir=$(dumpsys package $4 | grep dataDir | head -n 1 | cut -d '=' -f2)
-app_uid=$(dumpsys package $4 | grep userId= | head -n 1 | cut -d '=' -f2 | cut -d ' ' -f1)
+dataDir=$(dumpsys package ${package_name} | grep dataDir | head -n 1 | cut -d '=' -f2)
+app_uid=$(dumpsys package ${package_name} | grep userId= | head -n 1 | cut -d '=' -f2 | cut -d ' ' -f1)
 
 # sometimes app_uid has two lines. example: Google maps (com.google.android.apps.maps)
 # so "head -n 1" takes only the first line.
 # in android lollipop unnecessary extras along with app_uid. So take only first item.
 
-if [ ! -n "$app_uid" ]; then
-    echo "Failed to find package $4. Cannot restore data."
+if [[ -z "$app_uid" && -n "$(pm list packages ${package_name})" ]]; then
+    echo "Failed to find package $package_name. Cannot restore data."
 else
+
     rm -rf ${dataDir} 2>/dev/null
 
     # adding legacy support in v2.0
 
-    if [ ! -e /data/data/$3 ] && [ -e $1/$3 ]; then
-        cd $1
-        mv $3 /data/data/$3
+    if [[ -e /data/data/${tar_gz_file} ]]; then
+
+        cd /data/data/
+        busybox_file=${TEMP_DIR_NAME}/busybox
+
+        if [[ -e ${busybox_file} ]]; then
+            ${busybox_file} tar -xzpf ${tar_gz_file}
+        else
+            echo "${tar_gz_file} not found under /data/data/"
+        fi
+
+        rm ${tar_gz_file}
+
+        if [[ -e ${dataDir} ]]; then
+            chmod 755 ${dataDir}
+            chown ${app_uid}:${app_uid} -Rf ${dataDir}
+            restorecon -RF ${dataDir} 2>/dev/null
+        else
+            echo "Data dir $dataDir for package $package_name not found!"
+            echo "Data file was: $tar_gz_file"
+        fi
+
+    else
+        echo "Data file $tar_gz_file was not found!"
     fi
 
-    cd /data/data/
-
-    busybox_file=$2/busybox
-    if [ ! -e ${busybox_file} ] && [ -e $1/busybox ]; then
-        busybox_file=$1/busybox
-    fi
-
-    ${busybox_file} tar -xzpf $3
-    rm $3
-
-    chmod 755 ${dataDir}
-    chown ${app_uid}:${app_uid} -Rf ${dataDir}
-
-    restorecon -RF ${dataDir} 2>/dev/null
 fi
 
 
