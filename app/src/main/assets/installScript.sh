9fa5bf5f4aad4e0ae50d2dc07870f17d131bd10d
SayantanRC <sayantan.kgp@gmail.com>
2019-10-04 00:01:22 +0530

    further optimised scripts.
    Signed-off-by: SayantanRC <sayantan.kgp@gmail.com>

--- app/src/main/assets/installScript.sh
+++ app/src/main/assets/installScript.sh
@@ -22,40 +22,38 @@ if [[ -e ${TEMP_DIR_NAME}/${apk_dir_name} ]]; then
         pm install -r -d -t ${base_apk_name}
     fi
 
-    split_count=$(ls -1 | grep "split" | grep ".apk" | wc -l)
-
-    if [[ ${split_count} -gt 0 && -n "$(pm list packages ${package_name})" ]]; then
-
-        ############## Made by Vijay ##############
-
-        # create install session
-        echo "Split: Creating Install Session"
-        if [[ -n ${installer_name} && ${installer_name} != "NULL" ]]; then
-            session=$(pm install-create -i ${installer_name} -p ${package_name} | cut -d'[' -f2 | cut -d']' -f1)
-        else
-            session=$(pm install-create -p ${package_name} | cut -d'[' -f2 | cut -d']' -f1)
-        fi
-
-        # add split apks
-        echo "Split: Adding Split Apks to Session"
-        for filename in ${full_apk_dir}/split_*.apk; do
-            pm install-write ${session} ${filename} ${full_apk_dir}/${filename} 2>/dev/null && echo "Split: Added ${filename}"
-        done
-
-        echo "Split: Installing Split Session (Be patient, it may take longer)"
-        pm install-commit ${session}
-        echo "Split: Done"
-
-        ############## Made by Vijay ##############
-
-    else
-        echo "No split apks. Split count: $split_count"
-    fi
-
-    sleep 0.5s
-
     if [[ -z "$(pm list packages ${package_name})" ]]; then
         echo "App not installed. Please install manually via Google Play or elsewhere"
+    else
+
+        split_count=$(ls -1 | grep "split" | grep ".apk" | wc -l)
+        if [[ ${split_count} -gt 0 ]]; then
+
+            ############## Made by Vijay ##############
+
+            # create install session
+            echo "Split: Creating Install Session"
+            if [[ -n ${installer_name} && ${installer_name} != "NULL" ]]; then
+                session=$(pm install-create -i ${installer_name} -p ${package_name} | cut -d'[' -f2 | cut -d']' -f1)
+            else
+                session=$(pm install-create -p ${package_name} | cut -d'[' -f2 | cut -d']' -f1)
+            fi
+
+            # add split apks
+            echo "Split: Adding Split Apks to Session"
+            for filename in ${full_apk_dir}/split_*.apk; do
+                pm install-write ${session} ${filename} ${full_apk_dir}/${filename} 2>/dev/null && echo "Split: Added ${filename}"
+            done
+
+            echo "Split: Installing Split Session (Be patient, it may take longer)"
+            pm install-commit ${session}
+            echo "Split: Done"
+
+            ############## Made by Vijay ##############
+
+        else
+            echo "No split apks. Split count: $split_count"
+        fi
     fi
 
     rm -rf "$full_apk_dir"
